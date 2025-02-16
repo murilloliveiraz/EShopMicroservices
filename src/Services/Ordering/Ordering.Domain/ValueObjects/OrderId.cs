2f17793cadb072ee5c134c9480033b70ee5ccc9a
Murillo Oliveira <murillo1047@gmail.com>
2025-02-15 21:51:23 -0300

    feat: enriching domain and business logic and adding events

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
@@ -0,0 +1,17 @@
+﻿namespace Ordering.Domain.ValueObjects
+{
+    public record OrderId
+    {
+        public Guid Value { get; }
+
+        private OrderId(Guid value) => Value = value;
+        public static OrderId Of(Guid value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            if (value == Guid.Empty)
+                throw new DomainException("OrderId can't be null.");
+
+            return new OrderId(value);
+        }
+    }
+}
